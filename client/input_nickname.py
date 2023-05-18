e88e452406136bc98575e179d7b031e8b94dd436
Vladyslav Volokitin <the.xirex@gmail.com>
2023-05-18 21:24:55 +0300

    Finalizing nick input, customizing styles

--- client/input_nickname.py
+++ client/input_nickname.py
@@ -1,35 +1,37 @@
-from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
 from PyQt5 import QtCore
+from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
 from PyQt5.QtGui import QIcon
 
 class CustomInputDialog(QDialog):
     def __init__(self, parent=None):
+
         super().__init__(parent)
+
         self.setWindowTitle("Nickname")
         
-        # Создание элементов интерфейса
+        # Creating interface elements
         self.label = QLabel("Enter your nickname:")
         self.line_edit = QLineEdit()
         self.button_ok = QPushButton("OK")
+
+        # Set window style
+        style_file = QtCore.QFile('input-nickname.css')
+        style_file.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text)
+        stream = QtCore.QTextStream(style_file)
+        stylesheet = stream.readAll()
+        self.setStyleSheet(stylesheet)
+        self.setFixedSize(180, 120)
         
-        # Установка фиксированного размера окна
-        self.setFixedSize(200, 120)
-        
-        self.setStyleSheet('input-nickname.css')
-        
-        # Создание компоновки и добавление элементов
+        # Creating layouts and adding elements
         layout = QVBoxLayout()
 
         layout.addWidget(self.label)
         layout.addWidget(self.line_edit)
         layout.addWidget(self.button_ok)
-
         self.setLayout(layout)
         
+        self.setWindowIcon(QIcon('chat.png'))
         self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowContextHelpButtonHint)
-        
-        icon = QIcon('chat.png')
-        self.setWindowIcon(icon)
 
         self.button_ok.clicked.connect(self.on_ok_clicked)
         
@@ -38,4 +40,4 @@ class CustomInputDialog(QDialog):
         if nickname:
             self.accept()
         else:
-            QMessageBox.critical(self, 'Error', 'Empty name. Please enter a nickname.')
+            QMessageBox.critical(self, 'Error', 'Empty name. \nPlease enter a nickname.')
